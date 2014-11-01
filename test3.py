245494be58083e933238eeb41e8da90a34a93018
NoamRa <noamraby@gmail.com>
2014-11-02 01:13:51 +0200

    3

--- test3.py
+++ test3.py
@@ -1,22 +1,31 @@
 import cv2, platform
 import numpy as np
-    
+#import gi.repository
 
-
+    # mms://194.90.203.111/cam2
     # ffmpeg -i mmst://194.90.203.111/cam2 -f rtsp rtsp://127.0.0.1:
     # ffplay -i mmst://194.90.203.111/cam2
     # E:\Downloads\ffmpeg-20140830-git-596636a-win64-static\bin
     # ffmpeg -i mmst://194.90.203.111/cam2 -ar 44100 -vcodec libx264  -r 25 -b:v 500k -f flv rtmp://127.0.0.1:1935
     # ffmpeg -i mmst://194.90.203.111/cam2 -f flv "rtp://127.0.0.1:1935/cam2"
 
+    
+    # vlc http://192.168.0.103:8080/cam2
+
+cam = "mms://194.90.203.111/cam"
+#cam = 0 # Use  local webcam.
+
+cap = cv2.VideoCapture(cam)
+if not cap:
+    print("!!! Failed VideoCapture: invalid parameter!")
 
 
-cap = cv2.VideoCapture("E:\\Downloads\\ffmpeg-20140830-git-596636a-win64-static\\bin\\out.mp4")
-#print cap
-
-while(cap.isOpened()):
+while(True):
     # Capture frame-by-frame
     ret, current_frame = cap.read()
+    if type(current_frame) == type(None):
+        print("!!! Couldn't read frame!")
+        break
 
     # Display the resulting frame
     cv2.imshow('frame',current_frame)
